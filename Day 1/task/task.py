c163b4ccae70062b58d364334406e66d91665633
igk190 <graciakleijnen@gmail.com>
2025-01-13 22:10:33 +0100

    Day 1 - Band Name Generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Greetings yee ole' wizard!\nCan't think of a band name? I got you!\n")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name is: " + city + " " + pet)
