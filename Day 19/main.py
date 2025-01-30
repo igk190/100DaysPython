1dabd106ded56590013f93b05764480ca9449f77
igk190 <graciakleijnen@gmail.com>
2025-01-30 17:50:49 +0100

    Day 19 - Turtle Racing Game Finished

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,7 +1,8 @@
-import turtle
 from turtle import Turtle, Screen
+import random
 
-from PIL.Image import blend
+
+is_race_on = False
 
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -29,15 +30,38 @@ for racer in turtle_list:
     racer.color(colors[color_index])
     color_index += 1
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on: # prevent while loop from starting up while user is still betting
 
+    for turtle in turtle_list:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've chose {user_bet} and won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've chose {user_bet} and lost. The {winning_color} turtle is the winner.")
+
+        rand_distance = random.randint(0, 10)  # inclusive, includes 10
+        turtle.forward(rand_distance) # edit this
 
 screen.exitonclick()
 
 """ Learnings
+
 1. Either use left or setheading to a new number.
+
 2. There is a clear message for the turtle, and for the screen. Clearscreen deletes everyting.
 Clear only deletes the drawing.
+
 3. Tried to create instances using a for loop with range(colors_list_length -1). Got a KeyError.
+
 4. Okay, guess I could've created a for loop with range to create all the turtles at once anyway.
+
+5. Angela used turtle_index in the range(0,6) which excluded 6 to give each turtle their y coordinate.
+All y_positions were placed in a list. Then, she used tim.goto(... y=y_positions[turtle_index])
+I'm not adjusting my program with this solution just yet. We don't need the turtles named, we can
+make them move through loops.
 """
