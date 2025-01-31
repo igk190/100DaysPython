f73d5a95067ced8435541b7bc43db78574c0459c
igk190 <graciakleijnen@gmail.com>
2025-01-31 19:17:36 +0100

    Day 20 & 21 - Snake Game, updated incorrect starting var reference on grid.

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -5,13 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-start_y = 0
+start_x = 0
 
 for i in range(0, 3):
     square = Turtle("square")
     square.color("white")
-    square.goto(start_y,0)
-    start_y -= 20
+    square.goto(start_x,0)
+    start_x -= 20
 
 
 
@@ -23,6 +23,7 @@ for i in range(0, 3):
 
 
 
-
+#why in the previous one she used a for loop to create all of them at once?
+# And here they are separately initialized? Okay she does this later in the video
 
 screen.exitonclick()
