30cfdad6809c06d55efe42ad060021db47b8a632
igk190 <graciakleijnen@gmail.com>
2025-02-03 09:50:29 +0100

    Day 20 & 21 - Still restructuring snake into separate class.

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -8,36 +8,13 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake = Snake()
+snake = Snake() # AHA it's goin to be ONE snake, one instane.
 
-start_x = 0
-segments = []
+game_is_on = True  
 
-for i in range(0, 3):   # all of this should go into Snake class
-    square = Turtle("square")
-    square.color("white")
-    square.penup()
-    square.goto(start_x,0)
-    start_x -= 20
-    segments.append(square)
-
-game_is_on = True  # main.py should end up looking like this
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    snake.move()
-    
-    for seg_num in range(len(segments) - 1, 0, -1):  # index 2, 1, 0
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-
-
-
-#why in the previous one she used a for loop to create all of them at once?
-# And here they are separately initialized? Okay she does this later in the video
+    # snake.move()
 
 screen.exitonclick()
--- Day 20 & 21/snake.py
+++ Day 20 & 21/snake.py
@@ -1,18 +1,35 @@
+
 class Snake:
-    def __init__(self):
-        self.resources = {
-            "water": 300,
-            "milk": 200,
-            "coffee": 100,
-        }
+    def __init__(self, shape="square", color="white", x=0, y=0, penup=True):
+        self.shape = shape
+        self.color = color
+        self.x = x
+        self.y = y
+        self.penup = penup
 
+    def create_snake(self):
+        start_x = 0
 
-    def move(self):
-        """ Moves the snake."""
+for i in range(0, 3):   # all of this should go into Snake class
+    square = Turtle("square")
+    square.color("white")
+    square.penup()
+    square.goto(start_x,0)
+    start_x -= 20
+    segments.append(square)
+
+    # def update_x_y(self):
+    #     #do sth
+    
+    # def move(self):
         # do sth
-            for seg_num in range(len(segments) - 1, 0, -1):  # index 2, 1, 0
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-       
+
+#     segments = []
+
+# for seg_num in range(len(segments) - 1, 0, -1):  # index 2, 1, 0
+#     new_x = segments[seg_num -1].xcor()
+#     new_y = segments[seg_num -1].ycor()
+#     segments[seg_num].goto(new_x, new_y)
+# segments[0].forward(20)
+    
+
