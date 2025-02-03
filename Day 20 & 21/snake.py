19b8ea173262aa3b67caceec72073f6117f8af96
igk190 <graciakleijnen@gmail.com>
2025-02-03 11:24:02 +0100

    Day 20 & 21 - Added up, down, left, right methods to Snake.

--- Day 20 & 21/snake.py
+++ Day 20 & 21/snake.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
-
+STEP_SIZE = 20
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.heading = 0
 
 
     def create_snake(self):
@@ -24,6 +25,22 @@ class Snake:
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(20)
+            self.segments[0].forward(STEP_SIZE)
         
+    def up(self):
+        self.segments[0].setheading(90)
+    def down(self):
+        self.segments[0].setheading(270)
+    def left(self):
+        self.segments[0].setheading(180)
+    def right(self):
+        self.segments[0].setheading(0)
 
+
+""" Learnings
+1. We only move the head of the snake. The other segments follow. 
+2.     def up(self):
+        self.segments = self.segments[0].setheading(90)
+    --> we don't need to reassign self.segments, as setheading(90)
+    directly modifies the first element. See Learnings #1. 
+"""
