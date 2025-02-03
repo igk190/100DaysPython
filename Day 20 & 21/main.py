b2f31fe828c6bb92b345fd266bec6cc82296107e
igk190 <graciakleijnen@gmail.com>
2025-02-03 22:04:15 +0100

    Day 20 & 21 - Added methods that prevent Snake from going in the opposite direction.

--- Day 20 & 21/main.py
+++ Day 20 & 21/main.py
@@ -20,7 +20,7 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
     snake.move()
 
 screen.exitonclick()
--- Day 20 & 21/snake.py
+++ Day 20 & 21/snake.py
@@ -1,12 +1,15 @@
 from turtle import Turtle
 STEP_SIZE = 20
-
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-        self.heading = 0
+        self.head = self.segments[0]
 
 
     def create_snake(self):
@@ -25,22 +28,36 @@ class Snake:
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(STEP_SIZE)
+            self.head.forward(STEP_SIZE)
         
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
 """ Learnings
 1. We only move the head of the snake. The other segments follow. 
+
 2.     def up(self):
         self.segments = self.segments[0].setheading(90)
     --> we don't need to reassign self.segments, as setheading(90)
     directly modifies the first element. See Learnings #1. 
+
+3. Now it makes sense: we need one snake body, to which we add elements.
+    Only the first segment determines the direction. The rest just follows
+    the path of segments before it.
+
+4. It makes sense to add an attribute that refers to segments[0]. Makes 
+    the code shorter/cleaner. (I wrote self.segments[0].setheading(direction))
+
+5. Check how you name attributes and functions when using modules! "Heading" is a Turtle method.
 """
