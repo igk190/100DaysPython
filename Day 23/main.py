73eff23801b56a3e0481bdfa5a62f5619ed738c0
igk190 <graciakleijnen@gmail.com>
2025-02-17 20:48:37 +0100

    Day 23 - Initialized a Player, set starting attributes.

--- Day 23/main.py
+++ Day 23/main.py
@@ -8,17 +8,23 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+
+screen.onkey(player.move_up, "Up") 
+
+
+
 """3 Create a turtle player that starts at the bottom of the screen
- and listen for the "Up" keypress to move the turtle north. 
- If you get stuck, check the video walkthrough in Step 3.
+ and listen for the "Up" keypress to move the turtle north. stuck? Step 3.
 """
 
 """4 Create cars that are 20px high by 40px wide that are randomly
  generated along the y-axis and move to the left edge of the screen.
    No cars should be generated in the top and bottom 50px of the screen
-     (think of it as a safe zone for our little turtle).
-     Hint: generate a new car only every 6th time the game loop 
-     runs. If you get stuck, check the video walkthrough in Step 4."""
+     (think of it as a safe zone for our little turtle). Hint: generate a new car only every 
+     6th time the game loop runs. STuck? Step 4."""
 
 
 """Detect when the turtle player collides with a car and
