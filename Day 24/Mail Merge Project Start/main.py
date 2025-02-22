5914fa600947436a4f0fcd7f25833480251b0029
igk190 <graciakleijnen@gmail.com>
2025-02-22 22:04:55 +0100

    Day 24 - Mail Merger: Added learnings, fstring, and placeholder constant.

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -1,5 +1,6 @@
 invite = open("Input/Letters/starting_letter.txt")
 
+PLACEHOLDER = "[name]"
 invite_cleaned = []
 
 for line in invite.readlines():
@@ -17,12 +18,17 @@ for name in all_names:
     fname = name
     if " " in name:
         fname = name.replace(" ", "_") # if empty space in name, add underscore to filename
-    f = open("Output/ReadyToSend/letter_for_" + fname + ".txt", "w")
+    f = open(f"Output/ReadyToSend/letter_for_{fname}.txt", "w")
    
     for line in invite_cleaned:
-        if "[name]" in line: 
-            line = str(line).replace("[name]", name) # result of replace should be assigned to var!
-            print("BEGIN", line,"END")
+        if PLACEHOLDER in line: 
+            line = line.replace(PLACEHOLDER, name) # result of replace should be assigned to var!
         f.write(line)
 
 
+""" Learnings
+1. Line 24: Assign the result of str.replace() to a var.
+2. You could've used with open("filename.txt", "mode") as file: and then modified it.
+3. To strip empty spaces and also strip the newline "\n", apply strip() method.
+4. When you open a file that doesn't exist, Python will create it for you.
+"""
