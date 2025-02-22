098a1eec55bcfda6f85f95d8c39eb59c96f4013f
igk190 <graciakleijnen@gmail.com>
2025-02-22 21:30:25 +0100

    Day 24 - Separate folders for Snake With High Score and Mail Merge Project. Mail Merge: Take starter file, read names, create invitation for each name, save in separate files.

---
+++ Day 24/Snake with High Score/scoreboard.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 14, "normal")
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.hideturtle()
+        self.goto(0, 280)
+        self.writeScore() 
+
+    def writeScore(self): # write all scores to screen
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", False, ALIGNMENT, FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score # update high score if needed
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
+        self.score = 0  # reset score to zero
+        self.writeScore() # write to screen
+
+    def increaseScore(self):
+        self.score += 1   
+        self.writeScore()
+
+
+
+
+        
+    
+
+
