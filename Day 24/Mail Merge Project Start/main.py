5e15fdcf27f4d539536fae9ca947b1aaa6f7da70
igk190 <graciakleijnen@gmail.com>
2025-02-22 21:33:26 +0100

    Day 24 - Clean up folders Snake with High Score and Mail Merger.

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -1,8 +1,28 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+invite = open("Input/Letters/starting_letter.txt")
+
+invite_cleaned = []
+
+for line in invite.readlines():
+    invite_cleaned.append(line)
+
+print(invite_cleaned)
+
+names = open("Input/Names/invited_names.txt")
+all_names = names.readlines()
+print(all_names)
+
+for name in all_names:
+
+    name = name.strip("\n")
+    fname = name
+    if " " in name:
+        fname = name.replace(" ", "_") # if empty space in name, add underscore to filename
+    f = open("Output/ReadyToSend/letter_for_" + fname + ".txt", "w")
+   
+    for line in invite_cleaned:
+        if "[name]" in line: 
+            line = str(line).replace("[name]", name) # result of replace should be assigned to var!
+            print("BEGIN", line,"END")
+        f.write(line)
+
+
