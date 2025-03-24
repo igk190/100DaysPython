2b5c52e09c7a849040fd3773e61a26b07f26834d
igk190 <graciakleijnen@gmail.com>
2025-03-24 17:35:09 +0100

    Day 29 - Tkinter Password Manager, save entries to text file when clicking Add to MyPass, clear entry fields.

--- Day 29/main.py
+++ Day 29/main.py
@@ -3,6 +3,38 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+
+def save():
+
+    f = open("data.txt", "a+")
+    
+    website_value = website_entry.get()
+    email_value = email_entry.get()
+    password_value = password_entry.get()
+
+    f.write(f'\n{website_value} | {email_value} | {password_value}')
+    content = f.readlines()
+    print(content)
+    f.close()
+
+    website_entry.delete(0, 'end')
+    email_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
+
+
+
+
+
+
+# save that data into file called data.text
+# take all 3 with space in between and pipe symbol or staticmethod
+# on press button, trigger function that saves info in data.txt File
+# append file on new line underneath previous entry
+
+# once press add, all of info except email cleared OUTSIDE
+# look at delete finctoion entirely from an entry
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -19,23 +51,25 @@ website_label = Label(text="Website:", fg="black", bg="white")
 website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35, bg="white", fg="black", highlightthickness=0, borderwidth=1)
-website_entry.grid(column=1, row=1, columnspan=1)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_user_label = Label(text="E-mail/Username:", fg="black", bg="white")
 email_user_label.grid(column=0, row=2)
 
 email_entry = Entry(width=35, bg="white", fg="black", highlightthickness=0, borderwidth=1)
-email_entry.grid(column=1, row=2)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "hello@email.com")
 
 password_label = Label(text="Password:", fg="black", bg="white")
 password_label.grid(column=0, row=3)
-password_entry = Entry(width=21, bg="white", fg="black", highlightthickness=0, borderwidth=1)
-password_entry.grid(column=1, row=3, columnspan=1)
+password_entry = Entry(width=18, bg="white", fg="black", highlightthickness=0, borderwidth=1)
+password_entry.grid(column=1, row=3)
 generate_pw_btn = Button(text="Generate password", highlightbackground="white", anchor="e")
 generate_pw_btn.grid(column=2, row=3)
 
-add_btn = Button(text="Add to MyPass", width=36, highlightbackground="white")
-add_btn.grid(column=1, row=4, columnspan=1)
+add_btn = Button(text="Add to MyPass", width=32, highlightbackground="white", command=save)
+add_btn.grid(column=1, row=4, columnspan=2)
 
 
 
