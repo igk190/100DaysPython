c2817aefb0514b7291cb74cc3941c6165a96a24e
igk190 <graciakleijnen@gmail.com>
2025-04-02 16:05:52 +0200

    Day 31 - Tkinter Flashcard App, finished setting up front card UI.

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
+PT_FONT = ("Arial", 40, "italic")
+EN_FONT = ("Arial", 60, "bold")
 
 window = Tk() 
 window.title("Flashy")
@@ -12,9 +14,22 @@ card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 262, image=card_front)
 canvas.grid(column=0, row=0, columnspan=2)
 
-wrong_img = PhotoImage(file="images/wrong.png")
-cross_button = Button(image=wrong_img, highlightthickness=0)
+canvas.create_text(400, 150, text="Portuguese Word", fill="black", font=PT_FONT)
+canvas.create_text(400, 263, text="English word", fill="black", font=EN_FONT)
+
+
+# pt_text = Label(text="Portuguese word", fg="black", bg="white", font=PT_FONT)
+# pt_text.grid(column=0, row=0)
+# en_text = Label(text="English word", fg="black", bg="white", font=EN_FONT)
+# en_text.grid(column=0, row=1)
+
+wrong_icon = PhotoImage(file="images/wrong.png")
+cross_button = Button(image=wrong_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
 cross_button.grid(column=0, row=1,)
+
+right_icon = PhotoImage(file="images/right.png")
+right_button = Button(image=right_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+right_button.grid(column=1, row=1,)
 
 
 
