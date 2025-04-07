cba1332ad96561e0a3cb8c99d25fad0e3113c493
igk190 <graciakleijnen@gmail.com>
2025-04-07 22:14:56 +0200

    Day 31 - Tkinter Flashy, show card answer at first card without first needing to press any button (3 sec countdown that runs once).

--- Day 31/main.py
+++ Day 31/main.py
@@ -2,54 +2,61 @@ from tkinter import *
 import pandas as pd
 from random import randint
 
+
 BACKGROUND_COLOR = "#B1DDC6"
 PT_FONT = ("Arial", 40, "italic")
 EN_FONT = ("Arial", 60, "bold")
 
 # ------------------------ Read CSV ------------------------
 df = pd.read_csv('data/pt_en.csv')
-print(df["PT"].values[0])
-print(df["EN"].values[0])
+# print(df["PT"].values[0])
+
+current_card_index = randint(0, len(df)-1)
 
 
-# ------------------------ Get random PT word ------------------------
-def cant_recall():
-    canvas.itemconfig(pt_word, fill="green", text=df["PT"].values[randint(0, 1000)])
-
-def correct_guess():
-    canvas.itemconfig(pt_word, fill="green", text=df["PT"].values[randint(0, 1000)])
-
+# ------------------------ Show answer (flip card) ------------------------
+def show_answer():
+    canvas.itemconfig(canvas_image, image=card_back) # show back of card
+    canvas.itemconfig(lang_text, text="English translation", fill="white")
+    canvas.itemconfig(word_to_learn, fill="white", text=df["EN"].values[current_card_index])
 
 
+# ------------------------ Next card (get random PT word) ------------------------
+def next_card():
+    global current_card_index
+
+    current_card_index = randint(0, 1000) # reset random int, so show_answer can access it
+    canvas.itemconfig(canvas_image, image=card_front)
+    canvas.itemconfig(lang_text, text="Portuguese", fill="green")
+    canvas.itemconfig(word_to_learn, fill="black", text=df["PT"].values[current_card_index])
+
+    portuguese_word = df["PT"].values[current_card_index]
+
+
+    window.after(3000, show_answer)
 
 window = Tk() 
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=526,highlightthickness=0, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 262, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+
+canvas_image = canvas.create_image(400, 262, image=card_front)
 canvas.grid(column=0, row=0, columnspan=2)
 
-pt_text = canvas.create_text(400, 150, text="Portuguese", fill="black", font=PT_FONT)
-pt_word = canvas.create_text(400, 263, text=df["PT"].values[randint(0, 1000)], fill="black", font=EN_FONT)
+lang_text = canvas.create_text(400, 150, text="Portuguese", fill="green", font=PT_FONT)
+word_to_learn = canvas.create_text(400, 263, text=df["PT"].values[current_card_index], fill="black", font=EN_FONT)
+window.after(3000, show_answer)
 
 wrong_icon = PhotoImage(file="images/wrong.png")
-cross_button = Button(image=wrong_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=cant_recall)
+cross_button = Button(image=wrong_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
 cross_button.grid(column=0, row=1)
 
 right_icon = PhotoImage(file="images/right.png")
-right_button = Button(image=right_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=correct_guess)
+right_button = Button(image=right_icon, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
 right_button.grid(column=1, row=1)
-
-
-
-
-
-
-
-
-
 
 
 
