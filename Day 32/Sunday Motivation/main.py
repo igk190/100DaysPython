5fe775d9abea3220cf91c324351423c12fd55278
igk190 <graciakleijnen@gmail.com>
2025-05-25 19:53:04 +0200

    Day 32 - Move Sunday Motivation to own folder, starting project Birthday Wisher.

--- Day 32/Sunday Motivation/main.py
+++ Day 32/Sunday Motivation/main.py
@@ -7,9 +7,7 @@ year = now.year
 month = now.month
 day = now.weekday() # starts counting from zero
 
-# print(f"{day}-{month}-{year}")
 # date_of_birth = dt.datetime(year=1995, month=12, day=15,hour=4)
-# print(date_of_birth)
 
 all_quotes = [] # will have 102 lines
 
@@ -18,7 +16,7 @@ with open("quotes.txt") as quotes:
         all_quotes.append(line)
 
 my_email = "xyz@gmail.com"
-password = "abcdef123"
+password = "abcd1234()"
 
 if day == 6:
     print("wow it's sunday")
@@ -27,13 +25,21 @@ if day == 6:
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email, 
-            to_addrs="someonesemail@hotmail.com", 
+            to_addrs="abc@hotmail.com", 
             msg=f"Subject:Sunday Motivation\n\n{all_quotes[randint(0, len(all_quotes)-1)]}"
             # msg=f"Subject:Sunday Motivation\n\n{all_quotes[0]}"
             )
 
 
 
+
+""" Learnings
+1. Instead of adding lines to a new list, with open("quotes.txt") as file: 
+    all_qutotes = file.readlines()
+    quote = random.choice(all_quotes)
+
+2. Add content in msg with 2 newlines \n\n.
+"""
 
 
 
