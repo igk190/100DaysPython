a66b108f5db2b3be0390e2f4e607639fdd7bf521
igk190 <graciakleijnen@gmail.com>
2025-05-29 12:27:33 +0200

    Day 32 - If dataframe contains multiple people who have birthday today, send each person an email.

--- Day 32/Birthday Wisher/main_bday.py
+++ Day 32/Birthday Wisher/main_bday.py
@@ -6,17 +6,16 @@
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv ✔️
 
-# 4. Send the letter generated in step 3 to that person's email address. 
+# 4. Send the letter generated in step 3 to that person's email address. ✔️
 
-# 5. Check if df > 1 person has a bday, if so, send email to each.
+# 5. Check if df > 1 person has a bday, if so, send email to each. ✔️
 
 import datetime as dt
 import random
 import smtplib
 import pandas as pd
 
-now = dt.datetime.now()
-# print(now.day) # date of the month 
+now = dt.datetime.now() # now.day for date of the month
 
 bdays = pd.read_csv("birthdays.csv", usecols=["name","email","month","date"]) # returns pandas df, defines cols to use
 
@@ -27,47 +26,37 @@ bday_persons = bdays[(bdays['month'] == now.month) & (bdays['date'] == now.day)]
 letters = ["letter_1.txt","letter_2.txt","letter_3.txt"]
 
 my_email = "abc@gmail.com"
-password = "abc"
+password = "blabla"
 
-if len(bday_persons) != 0:
-    print(len(bday_persons))
-    print("Omg someone has their bday!")
+if len(bday_persons) == 0:
+    print("No parties today.😢")
+elif len(bday_persons) >= 1:
+    print("Get your gifts ready, put on your party hat🥳!\n")
 
-    bday_letter = random.choice(letters)
-    file_path = "letter_templates/" + bday_letter 
-
-    with open(file_path, "r") as letter:
-        search_text = "[NAME]"
-        replace_text = bday_persons["name"].iloc[0]
-        # print(replace_text) # Henkie
+    for index, row in bday_persons.iterrows():
+        print(row["name"], "has their bday!\n")
+        bday_letter = random.choice(letters)
+        file_path = "letter_templates/" + bday_letter 
         
-        contents = letter.read()
-        contents = contents.replace(search_text, replace_text)
-        # print(contents)
+        with open(file_path, "r") as letter:
+            search_text = "[NAME]"
+            replace_text = bday_persons["name"].iloc[index] 
+        
+            contents = letter.read()
+            contents = contents.replace(search_text, replace_text)
 
-        # with smtplib.SMTP("smtp.gmail.com") as connection: 
-        #     connection.starttls() # ensures encryption
-        #     connection.login(user=my_email, password=password)
-        #     connection.sendmail(
-        #         from_addr=my_email, 
-        #         to_addrs="abc@hotmail.com", 
-        #         msg=f"Subject:Happy bday, {replace_text}\n\n{contents}"
-        #         )
-else:
-    print("No parties today.")
+            with smtplib.SMTP("smtp.gmail.com") as connection: 
+                connection.starttls() # ensures encryption
+                connection.login(user=my_email, password=password)
+                connection.sendmail(
+                    from_addr=my_email, 
+                    to_addrs="abcabc@hotmail.com", 
+                    msg=f"Subject:Happy bday, {replace_text}\n\n{contents}"
+                    )
+
 
 # bday_person = bdays[(bdays["month"] == now.month) & (bdays["date"] == now.date)]
 # bday_person = bdays[bdays['month'] == now.month]
-
-
-
-
-
-
-
-
-# now = dt.datetime.now()
-# print(now.year)
 
 
 """ Learnings
@@ -81,6 +70,10 @@ https://stackoverflow.com/questions/17071871/how-do-i-select-rows-from-a-datafra
 3. 10 Ways to filter a pandas dataframe: https://www.listendata.com/2019/07/how-to-filter-pandas-dataframe.html
 
 4. Search and replace text in a Python file: https://www.geeksforgeeks.org/how-to-search-and-replace-text-in-a-file-in-python/ 
+
+5. Get a specific row in df: https://www.geeksforgeeks.org/get-a-specific-row-in-a-given-pandas-dataframe/
+
+6. Loop through rows in a df: https://www.freecodecamp.org/news/how-to-iterate-over-rows-with-pandas-loop-through-a-dataframe/ 
 
 """
 
