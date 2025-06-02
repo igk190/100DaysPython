aaa6493fb81f0e088999cea7b7a388c046ad458c
igk190 <graciakleijnen@gmail.com>
2025-06-02 19:12:44 +0200

    Day 33 - Kanye Quote Generator, add background color to canvas.

--- Day 33/Kanye Quote Generator/main_kanye.py
+++ Day 33/Kanye Quote Generator/main_kanye.py
@@ -11,16 +11,17 @@ def get_quote():
 
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg="white")
 
-canvas = Canvas(width=300, height=414)
+canvas = Canvas(width=300, height=414, highlightthickness=0)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
+canvas.config(background="white")
 quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightbackground="white", borderwidth=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
 
