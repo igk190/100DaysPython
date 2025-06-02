4c0b35b452b003a35564239498e80b7cf5f34ba2
igk190 <graciakleijnen@gmail.com>
2025-06-02 11:25:17 +0200

    Day 5 - Password Generator, fix index out of range error.

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -7,29 +7,28 @@ password = []
 letters_length = len(letters) #54, index 53
 numbers_length = len(numbers) #10, index 9
 symbols_length = len(symbols) #9, index 8
-# take random number 0-length of a list
-very_random_index = random.randint(0, numbers_length) #not including last index, so keep this
 
 # for i in range(0,symbols_length):
 #     print(symbols[i])
 
 print("Welcome to the PyPassword Generator!")
+
 nr_letters = int(input("How many letters would you like in your password?\n"))
 for i in range(0,nr_letters):
-    random_int = random.randint(0,letters_length) # get random value inside list length
+    random_int = random.randint(0,letters_length-1) # get random value inside list length
     password.append(letters[random_int]) # add value to password at random_int index
     print(password)
 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 for i in range(0,nr_symbols):
-    random_int_symbol = random.randint(0,symbols_length) # get random value inside list length
+    random_int_symbol = random.randint(0,symbols_length-1) # get random value inside list length
     print(random_int_symbol)
     password.append(symbols[random_int_symbol]) # add value to password at random_int index
     print(password)
 
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 for i in range(0,nr_numbers):
-    random_int_numbers = random.randint(0,numbers_length) # get random value inside list length
+    random_int_numbers = random.randint(0,numbers_length-1) # get random value inside list length
     print(random_int_numbers)
     password.append(numbers[random_int_numbers]) # add value to password at random_int index
     print(password)
