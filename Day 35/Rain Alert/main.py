907ecf31841e19fbeea4d3758bc7e4cef7ab5ed8
igk190 <graciakleijnen@gmail.com>
2025-06-04 00:45:52 +0200

    Day 35 - Send SMS and WhatsApp messages with Twilio if weather forecast predicts rain.

--- Day 35/Rain Alert/main.py
+++ Day 35/Rain Alert/main.py
@@ -1,22 +1,63 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "topsecret"
+api_key = "geheim"
+account_sid = "geheim"
+auth_token = "geheim"
 
-latitude = 52.520008
-longitude = 13.404954
+latitude = 41.649693
+longitude = -0.887712
 
 weather_params = {
     "lat": latitude,
     "lon": longitude,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 # response = requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={api_key}")
 
 response = requests.get(OWM_endpoint, params=weather_params)
 response.raise_for_status()
-data = response.json()
-for item in data["list"]:
-    print(item["weather"][0]["id"], ":", item["weather"][0]["description"])
+print(response.status_code)
+weather_data = response.json()
+
+rain_is_coming = False
+
+for item in weather_data["list"]:
+    print(item["weather"][0]["id"], ":", item["weather"][0]["description"],
+          item["dt_txt"])
+    if int(item["weather"][0]["id"])  < 700:
+        rain_is_coming = True
+
+if rain_is_coming:
+    proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+    body="It's gonna rain, gotta bring an umbrella!☂️",
+    from_="+geheim",
+    to="+geheim")
+    print(message.status)
+
+    # message = client.messages.create(
+    # from_="whatsapp:+geheim",
+    # body="It's going to rain today. Remember to bring an umbrella",
+    # to="whatsapp:+geheim"
+    # )
+
+    
+
+""" Learnings
+
+1. Instead of requests.get(url="..."), save the API endpoint as a string.
+Save parameters separately in a dict. Then do requests.get(url=API_Endpoint, params=weather_params)
+to keep things cleaner.
+
+2. Access a list with the index, and a dict with the name of the key as a string.
+"""
 
