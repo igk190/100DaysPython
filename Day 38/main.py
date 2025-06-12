aad4dc97477b35bbb57a0a9e73c4bb55ac51c4f2
igk190 <graciakleijnen@gmail.com>
2025-06-13 00:32:25 +0200

    Day 38 - Post workout logs from terminal straight to spreadsheet with Nutritionix and Sheety API.

--- Day 38/main.py
+++ Day 38/main.py
@@ -1,13 +1,18 @@
 import os
 import requests
+import datetime as dt
+import json
 
 NUTRITIONIX_APP_ID = os.getenv("NUTRITIONIX_APP_ID")
 NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
 NATTY_EXERCISE_ENDPOINT = "v2/natural/exercise"
-full_endpoint = f"{HOST_DOMAIN}/{NATTY_EXERCISE_ENDPOINT}"
+NATTY_FULL_ENDPOINT = f"{HOST_DOMAIN}/{NATTY_EXERCISE_ENDPOINT}"
+SHEETY_BEARER_TOKEN = os.getenv("SHEETY_BEARER_TOKEN")
 
-headers = {
+sheety_url = os.getenv("SHEETY_ENDPOINT")
+
+nutri_headers = {
     "x-app-id": NUTRITIONIX_APP_ID,
     "x-app-key": NUTRITIONIX_API_KEY,
     "Content-Type": "application/json",
@@ -17,8 +22,33 @@ params = {
     "query": input("What exercises did you do?") # only param required
 }
 
-response = requests.post(url=full_endpoint, headers=headers, json=params)
-print(response.text)
+response = requests.post(url=NATTY_FULL_ENDPOINT, headers=nutri_headers, json=params)
+resp_json = response.json()["exercises"]
+
+for index, item in enumerate(resp_json):
+
+    payload = {
+        "workout": {
+            "date": dt.datetime.now().strftime("%x"),
+            "time": dt.datetime.now().strftime("%X"),
+            "exercise": resp_json[index]["name"],
+            "duration": resp_json[index]["duration_min"],
+            "calories": resp_json[index]["nf_calories"],
+        }
+    }
+    sheety_headers = {
+        "Authorization": f"Bearer {SHEETY_BEARER_TOKEN}"
+    }
+
+    sheety_r = requests.post(
+        sheety_url, 
+        json=payload, 
+        headers=sheety_headers)
+    
+    print("sheety.text", sheety_r.text)
+    print("json dumps", json.dumps(payload))
+
+
 
 """ Learnings
 
@@ -37,6 +67,13 @@ Aka: we can continye to insert the data and json in objects like in earlier exer
 Ex: requests.post(url, data = myobj, timeout=2.50)
 
 Here, of exercise params, only query is required.
+
+2. AI says: response.text gives you a string. You'd need to manually parse it with json.loads().
+response.json() gives you a ready-to-use dict --> and easier access to keys and values.
+
+3. I was using the correct logic. But on response.text instead of the .json().
+
+4. Using json=payload automatically sets Content-Type: application/json for me.
 
 """
 
